30851ea040f5f01a7057cd6e7947ded1d1697a13
Corey Alix <coreyalix@gmail.com>
2024-06-20 09:09:04 -0400

    light refactor

--- site/gl/index.ts
+++ site/gl/index.ts
@@ -1,5 +1,5 @@
-export { setupChartOfAccountsForm } from "./chart-of-accounts.js";
-export { setupGeneralLedgerForm } from "./gl.js";
+export { setupChartOfAccountsForm } from "./setupChartOfAccountsForm.js";
+export { setupGeneralLedgerForm } from "./setupGeneralLedgerForm.js";
 export { setupAccountHistoryForm } from "./setupAccountHistoryForm.js";
 export { setupPointOfSaleForm } from "./setupPointOfSaleForm.js";
 export { setupGeneralLedgerWelcomeForm } from "./setupGeneralLedgerWelcomeForm.js";
