59f62182bb9e0c3063e25b8260e589e26b77f759
Corey Alix <coreyalix@gmail.com>
2024-06-21 05:56:27 -0400

    re-balance accounts

---
+++ scripts/compute_account_totals.mjs
@@ -0,0 +1,27 @@
+// save mb2024 to a file
+import { readFileSync } from "fs";
+
+const fileName = "../server/data/mb2024.json";
+const mb2024 = JSON.parse(readFileSync(fileName, "utf8"));
+
+const totals = {};
+mb2024.batches.forEach((batch) => {
+  batch.transactions.forEach((transaction) => {
+    if (!totals[transaction.account]) {
+      totals[transaction.account] = 0;
+    }
+    totals[transaction.account] += transaction.amt;
+  });
+});
+
+mb2024.accounts.forEach((account) => {
+  if (totals[account.id]) {
+    account.balance = parseFloat(totals[account.id].toFixed(2));
+  }
+});
+
+console.log(totals);
+
+// save mb2024 to a file
+import { writeFileSync } from "fs";
+writeFileSync(fileName, JSON.stringify(mb2024));
