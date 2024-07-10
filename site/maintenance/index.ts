308c7d3ac06c4cd84f5e05f13c62e75014abf5ba
Corey Alix <CoreyAlix@hotmail.com>
2024-07-10 09:49:50 -0400

    add comment to chlorine readings

--- site/maintenance/index.ts
+++ site/maintenance/index.ts
@@ -2,9 +2,21 @@ import { getElements } from "../fun/index.js";
 import { database } from "../db/index.js";
 
 export async function setupFreeChlorineForm() {
+
+  function updateReadings() {
+    const readings = database.getFreeChlorine();
+    [...readings].reverse().forEach((reading) => {
+      const row = ux.freeChlorineReadings.insertRow();
+      row.insertCell().textContent = reading.date;
+      row.insertCell().textContent = reading.freeChlorine.toString();
+      row.insertCell().textContent = reading.location;
+      row.insertCell().textContent = reading.comment;
+    });
+  }
   await database.init();
 
   const ux = {
+    freeChlorineForm: null as any as HTMLFormElement,
     freeChlorine: null as any as HTMLInputElement,
     date: null as any as HTMLInputElement,
     location: null as any as HTMLInputElement,
@@ -17,20 +29,15 @@ export async function setupFreeChlorineForm() {
 
   ux.date.valueAsDate = new Date();
 
-  ux.submit.addEventListener("click", () => {
+  ux.submit.addEventListener("click", async () => {
     const freeChlorine = ux.freeChlorine.valueAsNumber;
     const date = ux.date.valueAsDate!.toISOString().split("T")[0];
     const location = ux.location.value;
     const comment = ux.comment.value;
-    database.addFreeChlorine({ freeChlorine, date, location, comment});
+    await database.addFreeChlorine({ freeChlorine, date, location, comment });
+    updateReadings();
+    ux.freeChlorineForm.reset();
   });
 
-  const readings = database.getFreeChlorine();
-  readings.forEach((reading) => {
-    const row = ux.freeChlorineReadings.insertRow();
-    row.insertCell().textContent = reading.date;
-    row.insertCell().textContent = reading.freeChlorine.toString();
-    row.insertCell().textContent = reading.location;
-    row.insertCell().textContent = reading.comment;
-  });
+  updateReadings();
 }
